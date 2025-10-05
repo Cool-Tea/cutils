a162aab504b55093369cf05644e141aa0279a7c3
cool-tea <943801990@qq.com>
2025-10-05 21:11:48 +0800

    Add typedef.h

---
+++ src/typedef.h
@@ -0,0 +1,16 @@
+#ifndef TYPEDEF_H
+#define TYPEDEF_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+typedef int32_t i32;
+typedef int64_t i64;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef size_t usize;
+
+typedef float f32;
+typedef double f64;
+
+#endif  // TYPEDEF_H
